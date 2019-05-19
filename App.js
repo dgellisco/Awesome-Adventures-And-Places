f7b67dec36531ab0b9e75a2836633b467ab184c1
David Ellis <dgellis@live.com>
2019-05-19 17:45:01 -0600

    Add text input and display of places

--- App.js
+++ App.js
@@ -7,23 +7,46 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    places: []
+  };
+  
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+        return {
+         places: prevState.places.concat(placeName)
+        };
+    });
+  }
+
+  placeDeletedHandler = index => {
+    this.setState(prevState => {
+      return {
+        // Checked all indexes.  If it is the index to be deleted, it will trigger false and not be added to new array.
+        places: prevState.places.filter((place, i) => {
+          return i !== index;
+        })
+      };
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <PlaceInput
+          onPlaceAdded={this.placeAddedHandler}
+        />
+        <PlaceList
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
       </View>
     );
   }
@@ -35,15 +58,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
