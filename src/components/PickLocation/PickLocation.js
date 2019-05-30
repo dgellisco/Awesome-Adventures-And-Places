908ca0837b7890c8653a02bfdd3923adf419b610
David Ellis <dgellis@live.com>
2019-05-29 19:24:12 -0600

    MapView functional

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Dimensions, StyleSheet, View } from 'react-native';
+
+import MapView from 'react-native-maps';
 
 class PickLocation extends Component {
+    state = {
+        // Set initial location
+        focusedLocation: {
+            latitude: 39.739235,
+            longitude: -104.990250,
+            latitudeDelta: 0.0122,
+            // Get the aspect ratio, multiply by latDelta
+            longitudeDelta:
+                Dimensions.get('window').width /
+                Dimensions.get('window').height *
+                0.0122
+        },
+        locationChosen: false
+    }
+
+    pickLocationHandler = event => {
+        const coords = event.nativeEvent.coordinate;
+        this.setState(prevState => {
+            return {
+                focusedLocation: {
+                    ...prevState.focusedLocation,
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                },
+                locationChosen: true
+            }
+        })
+    }
+
     render() {
+        let marker = null;
+
+        if (this.state.locationChosen) {
+            marker = <MapView.Marker coordinate={this.state.focusedLocation}/>
+        }
+
         return (
             <View style={styles.container}>
-                <View style={styles.placeholder}>
-                    <Text>Map</Text>
-                </View>
+                <MapView 
+                    initialRegion={this.state.focusedLocation}
+                    region={this.state.focusedLocation}
+                    style={styles.map}
+                    onPress={this.pickLocationHandler}
+                >
+                {/* The marker is either null, or the focused location */}
+                {marker}
+                </MapView>
                 <View style={styles.button}>
                     <Button
                         title="Locate Me"
@@ -24,12 +67,9 @@ const styles = StyleSheet.create({
         width: '100%',
         alignItems: 'center'
     },
-    placeholder: {
-        borderWidth: 1,
-        borderColor: 'black',
-        backgroundColor: '#eee',
-        width: '80%',
-        height: 150
+    map: {
+        width: '100%',
+        height: 250
     },
     button: {
         margin: 8
